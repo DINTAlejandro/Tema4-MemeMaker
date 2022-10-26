c73b6f6f4c55c720f7b5a72cd338076bd2222fd3
dam1alejandro <90453213+dam1alejandro@users.noreply.github.com>
2022-10-26 12:30:10 +0200

    Add files via upload

---
+++ Tema4-MemeMaker/MemeMaker/Convertidor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class Convertidor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch(value)
+            {
+                case (true):
+                    return 1;
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
